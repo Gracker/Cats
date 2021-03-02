9e8d504a4bc7fec6e0b065435106555e2f4083f7
Chris <dreamtale.jg@gmail.com>
2021-03-02 09:51:32 +0800

    apply spotlessCheck

--- app/src/main/java/com/example/androiddevchallenge/DetailPage.kt
+++ app/src/main/java/com/example/androiddevchallenge/DetailPage.kt
@@ -1,19 +1,37 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import android.util.Log
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.rememberScrollableState
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,21 +40,18 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
 
 class DetailPage {
     @Composable
-    fun detailPageScreen(navController: NavController, cat : Cats) {
+    fun detailPageScreen(navController: NavController, cat: Cats) {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Log.v("Gracker",cat.name)
+            Log.v("Gracker", cat.name)
             ArtistCard(cat)
         }
     }
@@ -45,12 +60,14 @@ class DetailPage {
     fun ArtistCard(
         cat: Cats
     ) {
-        Column(Modifier
-            .padding(PaddingValues(horizontal = 0.dp, vertical = 8.dp))
-            .verticalScroll(rememberScrollState())
+        Column(
+            Modifier
+                .padding(PaddingValues(horizontal = 0.dp, vertical = 8.dp))
+                .verticalScroll(rememberScrollState())
         ) {
             val padding = 8.dp
-            Row(verticalAlignment = Alignment.CenterVertically,
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(PaddingValues(horizontal = 0.dp, vertical = 2.dp))
@@ -70,10 +87,12 @@ class DetailPage {
                     painter = image, contentDescription = "",
                     contentScale = ContentScale.Crop
                 )
-                Column(Modifier
-                    .padding(padding)
-                    .weight(1f)
-                    .align(Alignment.CenterVertically)) {
+                Column(
+                    Modifier
+                        .padding(padding)
+                        .weight(1f)
+                        .align(Alignment.CenterVertically)
+                ) {
                     Text(cat.owner)
 
                     val randoms = (0..60).random()
@@ -81,9 +100,10 @@ class DetailPage {
                 }
             }
 
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(PaddingValues(horizontal = 0.dp, vertical = 2.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(PaddingValues(horizontal = 0.dp, vertical = 2.dp))
             ) {
                 val catImg: Painter = painterResource(id = cat.image)
                 Image(
@@ -101,14 +121,13 @@ class DetailPage {
                 )
             }
 
-            Column(Modifier
-                .padding(padding)
-            ){
+            Column(
+                Modifier
+                    .padding(padding)
+            ) {
                 val des = stringResource(cat.des)
                 Text(des)
             }
         }
     }
-
 }
-
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,17 +18,21 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androiddevchallenge.ui.theme.MyTheme
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,10 +40,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.google.gson.Gson
 import androidx.compose.foundation.clickable as clickable1
 
@@ -63,10 +73,13 @@ fun CatsFeedList(cats: List<Cats>, navController: NavController) {
     ) {
         items(cats.size) { index ->
 //            Text(text = "Name: ${cats[index].name}")
-            ArtistCard(cats[index], onClick = {
-                val catJson = Gson().toJson(cats[index])
-                navController.navigate("detail_screen/$catJson")
-            })
+            ArtistCard(
+                cats[index],
+                onClick = {
+                    val catJson = Gson().toJson(cats[index])
+                    navController.navigate("detail_screen/$catJson")
+                }
+            )
         }
     }
 }
@@ -76,12 +89,14 @@ fun ArtistCard(
     cat: Cats,
     onClick: () -> Unit,
 ) {
-    Column(Modifier
-        .padding(PaddingValues(horizontal = 0.dp, vertical = 8.dp))
-        .clickable1(onClick = onClick)
+    Column(
+        Modifier
+            .padding(PaddingValues(horizontal = 0.dp, vertical = 8.dp))
+            .clickable1(onClick = onClick)
     ) {
         val padding = 8.dp
-        Row(verticalAlignment = Alignment.CenterVertically,
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(PaddingValues(horizontal = 0.dp, vertical = 2.dp))
@@ -96,14 +111,17 @@ fun ArtistCard(
                     .border(
                         width = 2.dp,
                         color = Color.White,
-                        shape = RoundedCornerShape(20)),
+                        shape = RoundedCornerShape(20)
+                    ),
                 painter = image, contentDescription = "",
                 contentScale = ContentScale.Crop
             )
-            Column(Modifier
-                .padding(padding)
-                .weight(1f)
-                .align(Alignment.CenterVertically)) {
+            Column(
+                Modifier
+                    .padding(padding)
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+            ) {
                 Text(cat.owner)
 
                 val randoms = (0..60).random()
@@ -111,9 +129,10 @@ fun ArtistCard(
             }
         }
 
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(PaddingValues(horizontal = 0.dp, vertical = 2.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(PaddingValues(horizontal = 0.dp, vertical = 2.dp))
         ) {
             val catImg: Painter = painterResource(id = cat.image)
             Image(
@@ -164,13 +183,13 @@ fun ComposeNavigation() {
         }
         // detail page
         composable(
-                "detail_screen/{cat}",
-                arguments = listOf( navArgument("cat") { type = NavType.StringType })
+            "detail_screen/{cat}",
+            arguments = listOf(navArgument("cat") { type = NavType.StringType })
         ) { backStackEntry ->
             val mDetailPage = DetailPage()
             backStackEntry.arguments?.getString("cat")?.let { json ->
-                val cat = Gson().fromJson(json,Cats::class.java)
-                mDetailPage.detailPageScreen(navController = navController,cat)
+                val cat = Gson().fromJson(json, Cats::class.java)
+                mDetailPage.detailPageScreen(navController = navController, cat)
             }
         }
     }
@@ -180,13 +199,13 @@ fun ComposeNavigation() {
 fun MainPageScreen(navController: NavController) {
     Surface(color = MaterialTheme.colors.background) {
         var catsList: ArrayList<Cats> = ArrayList()
-        catsList.add(Cats("cats1", R.drawable.cats1, "Chris", R.drawable.owner1,R.string.cat1))
-        catsList.add(Cats("cats2", R.drawable.cats2, "Lee", R.drawable.owner2,R.string.cat2))
-        catsList.add(Cats("cats3", R.drawable.cats3, "Lina", R.drawable.owner3,R.string.cat3))
-        catsList.add(Cats("cats4", R.drawable.cats4, "Carl", R.drawable.owner4,R.string.cat4))
-        catsList.add(Cats("cats5", R.drawable.cats5, "Miksa", R.drawable.owner5,R.string.cat5))
-        catsList.add(Cats("cats6", R.drawable.cats6, "Gracker", R.drawable.owner6,R.string.cat6))
-        catsList.add(Cats("cats7", R.drawable.cats7, "Sam", R.drawable.owner7,R.string.cat7))
+        catsList.add(Cats("cats1", R.drawable.cats1, "Chris", R.drawable.owner1, R.string.cat1))
+        catsList.add(Cats("cats2", R.drawable.cats2, "Lee", R.drawable.owner2, R.string.cat2))
+        catsList.add(Cats("cats3", R.drawable.cats3, "Lina", R.drawable.owner3, R.string.cat3))
+        catsList.add(Cats("cats4", R.drawable.cats4, "Carl", R.drawable.owner4, R.string.cat4))
+        catsList.add(Cats("cats5", R.drawable.cats5, "Miksa", R.drawable.owner5, R.string.cat5))
+        catsList.add(Cats("cats6", R.drawable.cats6, "Gracker", R.drawable.owner6, R.string.cat6))
+        catsList.add(Cats("cats7", R.drawable.cats7, "Sam", R.drawable.owner7, R.string.cat7))
         CatsFeedList(catsList, navController)
     }
 }
